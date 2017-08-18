239d6372e152450b654e58be81bbbce33a4ccc87
simon <simon@journeyman.cc>
2017-08-18 14:08:34 +0100

    Still trying to get `lein release` to run cleanly.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject smeagol "0.99.7"
+(defproject smeagol "0.99.7-SNAPSHOT"
   :description "A simple Git-backed Wiki inspired by Gollum"
   :url "https://github.com/simon-brooke/smeagol"
   :license {:name "GNU General Public License,version 2.0 or (at your option) any later version"
@@ -71,6 +71,7 @@
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
                   ;; ["vcs" "tag"] -- not working, problems with secret key
+                  ["clean"]
                   ["bower" "install"]
                   ["ring" "uberwar"]
                   ["docker" "build"]
