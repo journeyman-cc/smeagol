9152d1fe6d5beb3428832cb155c89555174bb3b5
Simon Brooke <simon@journeyman.cc>
2017-09-12 01:14:45 +0100

    Version 0.99.11-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject smeagol "0.99.10"
+(defproject smeagol "0.99.11-SNAPSHOT"
   :description "A simple Git-backed Wiki inspired by Gollum"
   :url "https://github.com/simon-brooke/smeagol"
   :license {:name "GNU General Public License,version 2.0 or (at your option) any later version"
